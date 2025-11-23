a3ba0d23b48a96d9a56982bbd12caa9063fbee9e
alfonsoristorato <alfonsoristorato@gmail.com>
2025-11-23 17:50:04 +0000

    publish to gh packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
     alias(libs.plugins.gradle.ktlint)
     alias(libs.plugins.kotlin.jpa) // for test only
     alias(libs.plugins.kotlin.spring) // for test only
+    `maven-publish`
 }
 
 group = "alfonsoristorato"
+version = System.getenv("RELEASE_VERSION") ?: "LOCAL-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -42,5 +44,51 @@ configurations {
     all {
         exclude(group = "org.mockito", module = "mockito-core")
         exclude(group = "org.mockito", module = "mockito-junit-jupiter")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+
+            pom {
+                name.set("JPA Specification Kotlin DSL")
+                description.set("A Kotlin DSL for building JPA Specifications with idiomatic syntax")
+                url.set("https://github.com/alfonsoristorato/jpa-spec-kotlin-dsl")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/alfonsoristorato/jpa-spec-kotlin-dsl/blob/main/LICENSE")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("alfonsoristorato")
+                        name.set("Alfonso Ristorato")
+                        url.set("https://github.com/alfonsoristorato")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/alfonsoristorato/jpa-spec-kotlin-dsl.git")
+                    developerConnection.set("scm:git:ssh://github.com/alfonsoristorato/jpa-spec-kotlin-dsl.git")
+                    url.set("https://github.com/alfonsoristorato/jpa-spec-kotlin-dsl")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/alfonsoristorato/jpa-spec-kotlin-dsl")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
