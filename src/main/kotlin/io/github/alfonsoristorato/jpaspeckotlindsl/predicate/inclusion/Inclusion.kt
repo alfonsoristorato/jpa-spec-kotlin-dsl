3992db4ddc5d2adb58191fc91eb468bd2ba52d97
alfonsoristorato <alfonsoristorato@gmail.com>
2026-04-01 15:51:15 +0200

    containedIn alias for in

--- src/main/kotlin/io/github/alfonsoristorato/jpaspeckotlindsl/predicate/inclusion/Inclusion.kt
+++ src/main/kotlin/io/github/alfonsoristorato/jpaspeckotlindsl/predicate/inclusion/Inclusion.kt
@@ -23,6 +23,23 @@ fun <T, P> KProperty1<T, P>.`in`(
 ): Predicate = criteriaBuilder.`in`(path.get<P>(this.name)).value(value)
 
 /**
+ * Creates a [Predicate] that checks if the property's value is in the given value.
+ * This delegates to the [`in`] method.
+ *
+ * @receiver [T] – the type of the entity.
+ * @receiver [P] - the type of the property.
+ * @param path The path of the entity.
+ * @param criteriaBuilder The criteria builder.
+ * @param value The value to check for inclusion (can be a [Collection]).
+ * @return A [Predicate] that checks if the property is in the value.
+ */
+fun <T, P> KProperty1<T, P>.containedIn(
+    path: Path<T>,
+    criteriaBuilder: CriteriaBuilder,
+    value: P,
+): Predicate = `in`(path, criteriaBuilder, value)
+
+/**
  * Creates a [Predicate] that checks if the nested property's value is in the given value.
  *
  * @receiver [ROOT] – the root entity type.
@@ -37,3 +54,20 @@ fun <ROOT, PROP> NestedProperty<ROOT, PROP>.`in`(
     criteriaBuilder: CriteriaBuilder,
     value: PROP,
 ): Predicate = criteriaBuilder.`in`(resolve(path)).value(value)
+
+/**
+ * Creates a [Predicate] that checks if the nested property's value is in the given value.
+ * This delegates to the [`in`] method.
+ *
+ * @receiver [ROOT] – the root entity type.
+ * @receiver [PROP] - the type of the property.
+ * @param path The path of the root entity.
+ * @param criteriaBuilder The criteria builder.
+ * @param value The value to check for inclusion (can be a [Collection]).
+ * @return A [Predicate] that checks if the nested property is in the value.
+ */
+fun <ROOT, PROP> NestedProperty<ROOT, PROP>.containedIn(
+    path: Path<ROOT>,
+    criteriaBuilder: CriteriaBuilder,
+    value: PROP,
+): Predicate = `in`(path, criteriaBuilder, value)
