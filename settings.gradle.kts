0acbc6b2d3f813a3e189884caf7f5e1f6bac0582
alfonsoristorato <alfonsoristorato@gmail.com>
2026-05-10 21:35:08 +0100

    fix publishing settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,4 @@
 rootProject.name = "jpa-spec-kotlin-dsl"
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
 include(":jpa-spec-kotlin-dsl")
 include(":jpa-spec-kotlin-dsl-hibernate")
