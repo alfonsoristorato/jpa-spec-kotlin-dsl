0d2620a32f7ed02e8c04b21ebb014620bf503ca7
alfonsoristorato <alfonsoristorato@gmail.com>
2026-05-30 11:40:37 +0100

    HibernateCriteriaBuilder resolver

---
+++ jpa-spec-kotlin-dsl-hibernate/src/main/kotlin/io/github/alfonsoristorato/jpaspeckotlindslhibernate/internal/HibernateCriteriaBuilderUtil.kt
@@ -0,0 +1,9 @@
+package io.github.alfonsoristorato.jpaspeckotlindslhibernate.internal
+
+import jakarta.persistence.criteria.CriteriaBuilder
+import org.hibernate.query.criteria.HibernateCriteriaBuilder
+
+
+fun CriteriaBuilder.resolveHibernateCriteriaBuilder(): HibernateCriteriaBuilder =
+    this as? HibernateCriteriaBuilder
+        ?: throw IllegalStateException("CriteriaBuilder is not an instance of HibernateCriteriaBuilder. Ensure that you are using Hibernate as the JPA provider.")
